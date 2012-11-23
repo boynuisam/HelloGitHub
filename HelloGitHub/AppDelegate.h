9241067be112e14fdbc25934f74b12e53ce7bb1f
boynuisam <boynuisam@gmail.com>
2012-11-23 17:02:01 +0700

    Initial Commit

---
+++ HelloGitHub/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloGitHub
+//
+//  Created by NhutTruong on 11/23/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloGitHub
+//
+//  Created by NhutTruong on 11/23/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
