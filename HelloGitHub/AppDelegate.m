87e53d352e090fcc03a851278ef07ef316dea35b
boynuisam <boynuisam@gmail.com>
2012-11-23 18:00:16 +0700

    add new bug

--- HelloGitHub/AppDelegate.m
+++ HelloGitHub/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"My frst github"); 
+    NSLog(@"My frst github");123
     return YES;
 }
 							
