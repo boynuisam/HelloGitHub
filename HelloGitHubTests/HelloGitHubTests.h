97c53dcc2ce1880bde9441a78311b15634b4bb8a
nntruong <boydragon1588@gmail.com>
2012-11-26 14:13:22 +0700

    add unit test into project

---
+++ HelloGitHubTests/HelloGitHubTests.h
@@ -0,0 +1,13 @@
+//
+//  HelloGitHubTests.h
+//  HelloGitHubTests
+//
+//  Created by helios-team on 11/26/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloGitHubTests : SenTestCase
+
+@end
---
+++ HelloGitHubTests/HelloGitHubTests.m
@@ -0,0 +1,33 @@
+//
+//  HelloGitHubTests.m
+//  HelloGitHubTests
+//
+//  Created by helios-team on 11/26/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import "HelloGitHubTests.h"
+#import <SenTestingKit/SenTestingUtilities.h>
+@implementation HelloGitHubTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    //STFail(@"Unit tests are not implemented yet in HelloGitHubTests");
+    STAssertEquals(3, 1, @"Testing");
+}
+
+@end
