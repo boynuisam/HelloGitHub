8ea146e658a9e1d532f2a6327657d4f756e7ba47
nntruong <boydragon1588@gmail.com>
2012-11-27 10:43:34 +0700

    fix build

--- HelloGitHub/AppDelegate.m
+++ HelloGitHub/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"Add new line");121
+    NSLog(@"Add new line");
     return YES;
 }
 							
