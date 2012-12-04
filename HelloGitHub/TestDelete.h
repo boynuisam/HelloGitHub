a3a3a100553aeebc79080cd0b415614985c09d02
nntruong <boydragon1588@gmail.com>
2012-12-04 13:52:09 +0700

    addd test delete

---
+++ HelloGitHub/TestDelete.h
@@ -0,0 +1,13 @@
+//
+//  TestDelete.h
+//  HelloGitHub
+//
+//  Created by helios-team on 12/4/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestDelete : NSObject
+
+@end
---
+++ HelloGitHub/TestDelete.m
@@ -0,0 +1,13 @@
+//
+//  TestDelete.m
+//  HelloGitHub
+//
+//  Created by helios-team on 12/4/12.
+//  Copyright (c) 2012 NhutTruong. All rights reserved.
+//
+
+#import "TestDelete.h"
+
+@implementation TestDelete
+
+@end
