d043d9fb0fbde8a280c43b77860694de1f0b029a
TruongNguyen <truongnn@NhutTruong.local>
2015-03-14 15:57:11 +0700

    commit truongDev

--- HelloGitHub/AppDelegate.m
+++ HelloGitHub/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"Add new line");
+    NSLog(@"truongdev");
     return YES;
 }
 							
